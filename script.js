644dc64c778a74b248afd6010864c9ef7b5330fe
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-25 17:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,78 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+javascript
+Copy code
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const speedControl = player.querySelector('.speed__slider');
+const volumeControl = player.querySelector('.volume__slider');
+
+// Play/Pause functionality
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+
+// Skip buttons functionality
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+// Volume and Playback speed functionality
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// Progress bar functionality
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+video.addEventListener('timeupdate', handleProgress);
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+// Playback speed functionality
+function handleSpeed() {
+  video.playbackRate = this.value;
+}
+
+speedControl.addEventListener('change', handleSpeed);
+
+// Volume control functionality
+function handleVolume() {
+  video.volume = this.value;
+}
+
+volumeControl.addEventListener('change', handleVolume);
